7aaf418a3640b03f6aba0873a807b5f17dad3a91
Dan <dstein64@users.noreply.github.com>
2020-08-29 19:17:03 -0400

    Use an API agent to throttle requests.

--- main.js
+++ main.js
@@ -1,83 +1,120 @@
-// Do not set this above 100. That's the max permitted value, and the code below
+// Do not set PER_PAGE above 100. That's the max permitted value, and the code below
 // assumes there are no more items remaining if the total items returned is less
 // than the 'per_page' value specified.
 const PER_PAGE = 100;
 
-const process_repos = function(user, auth=null, callback=null, page=1) {
-    const xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState === 4 && this.status === 200) {
-            // The top level response is an array with items, unlike the other API calls that
-            // return a dictionary with 'total_count' along with an array of items.
-            const repos = JSON.parse(xhttp.responseText);
-            if (callback !== null) {
-                for (const repo of repos) {
-                    callback(repo);
+// An API agent is used as a way to throttle API calls, with the goal of preventing/reducing:
+//   > 403: You have triggered an abuse detection mechanism.
+//     Please wait a few minutes before you try again.
+//   > Chrome: net::ERR_INSUFFICIENT_RESOURCES
+const ApiAgent = function(auth=null) {
+    const API = 'https://api.github.com'
+
+    const request_limit = 2;
+    let num_requests = 0;
+    const pending = [];
+
+    const request = function(endpoint, callback=null) {
+        if (!endpoint.startsWith('/')) {
+            throw `invalid endpoint: ${endpoint}`
+        }
+        // TODO: turn on progress loading...
+        num_requests += 1;
+        const xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState === 4) {
+                num_requests -= 1;
+                if (this.status === 200 && callback != null) {
+                    const response = JSON.parse(xhttp.responseText);
+                    callback(response);
+                }
+                if (pending.length > 0) {
+                    if (num_requests < request_limit) {
+                        const {endpoint, callback} = pending.pop();
+                        request(endpoint, callback);
+                    }
+                } else if (pending.length === 0) {
+                    // TODO: turn off progress loading...
+                    // THIS CAN FIRE MULTIPLE TIMES
                 }
             }
-            if (repos.length === PER_PAGE) {
-                process_repos(user, auth, callback, page + 1);
+        };
+        const url = `${API}${endpoint}`;
+        xhttp.open('GET', url);
+        xhttp.setRequestHeader('Accept', 'application/vnd.github.v3+json');
+        if (auth !== null)
+            xhttp.setRequestHeader('Authorization', auth);
+        xhttp.send();
+    };
+
+    this.submit = function(endpoint, callback=null) {
+        if (num_requests < request_limit) {
+            request(endpoint, callback);
+        } else {
+            pending.push({endpoint: endpoint, callback: callback});
+        }
+    }
+};
+
+const API_AGENT = new ApiAgent(null);
+
+const process_repos = function(user, callback=null, page=1) {
+    const request_callback = function(repos) {
+        // The top level response is an array with items, unlike the other API calls that
+        // return a dictionary with 'total_count' along with an array of items.
+        if (callback !== null) {
+            for (repo of repos) {
+                callback(repo);
             }
+        }
+        if (repos.length === PER_PAGE) {
+            process_repos(user, callback, page + 1);
         }
     };
     const params = new URLSearchParams({
         page: page,
         per_page: PER_PAGE,
     });
-    const url = `https://api.github.com/users/${user}/repos?` + params.toString();
-    xhttp.open('GET', url);
-    xhttp.setRequestHeader('Accept', 'application/vnd.github.v3+json');
-    if (auth !== null)
-        xhttp.setRequestHeader('Authorization', auth);
-    xhttp.send();
+    const endpoint = `/users/${user}/repos?` + params.toString();
+    API_AGENT.submit(endpoint, request_callback);
 };
 
-const process_workflows = function(user, repo, auth=null, callback=null, page=1, count=0) {
-    const xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState === 4 && this.status === 200) {
-            const response = JSON.parse(xhttp.responseText);
-            const total_count = response.total_count;
-            const workflows = response.workflows;
-            count += workflows.length;
-            if (callback !== null) {
-                for (const workflow of workflows) {
-                    callback(workflow);
-                }
+const process_workflows = function(user, repo, callback=null, page=1, count=0) {
+    const request_callback = function(response) {
+        const total_count = response.total_count;
+        const workflows = response.workflows;
+        count += workflows.length;
+        if (callback !== null) {
+            for (const workflow of workflows) {
+                callback(workflow);
             }
-            if (count < total_count) {
-                process_workflows(user, repo, auth, callback, page + 1, count);
-            }
+        }
+        if (count < total_count) {
+            process_workflows(user, repo, callback, page + 1, count);
         }
     };
     const params = new URLSearchParams({
         page: page,
         per_page: PER_PAGE,
     });
-    const url = `https://api.github.com/repos/${user}/${repo}/actions/workflows?` + params.toString();
-    xhttp.open('GET', url);
-    xhttp.setRequestHeader('Accept', 'application/vnd.github.v3+json');
-    if (auth !== null)
-        xhttp.setRequestHeader('Authorization', auth);
-    xhttp.send();
+    const endpoint = `/repos/${user}/${repo}/actions/workflows?` + params.toString();
+    API_AGENT.submit(endpoint, request_callback);
 };
 
-const process_run = function(user, repo, workflow_id, auth=null, callback=null) {
-    const xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState === 4 && this.status === 200) {
-            const response = JSON.parse(xhttp.responseText);
-            if (response.total_count > 0 && response.workflow_runs.length >= 1) {
-                if (callback !== null) callback(response.workflow_runs[0]);
-            }
+const process_run = function(user, repo, workflow_id, branch=null, callback=null) {
+    const request_callback = function(response) {
+        if (response.total_count > 0 && response.workflow_runs.length >= 1) {
+            if (callback !== null) callback(response.workflow_runs[0]);
         }
-    };
-    const url = `https://api.github.com/repos/${user}/${repo}/actions/workflows/${workflow_id}/runs?per_page=1`;
-    xhttp.open('GET', url);
-    xhttp.setRequestHeader('Accept', 'application/vnd.github.v3+json');
-    if (auth !== null)
-        xhttp.setRequestHeader('Authorization', auth);
-    xhttp.send();
+    }
+    const params = new URLSearchParams({
+        per_page: 1,
+    });
+    if (branch !== null) {
+        params.append('branch', branch);
+    }
+    const endpoint = `/repos/${user}/${repo}/actions/workflows/${workflow_id}/runs?` + params.toString();
+    API_AGENT.submit(endpoint, request_callback);
 };
 
 // Returns the index in the table for inserting a new row, such that alphabetic ordering
@@ -105,8 +142,7 @@ const get_idx = function(repo, workflow) {
 };
 
 const user = 'dstein64';
-const auth = null;
-process_repos(user, auth, (repo) => {
+process_repos(user, (repo) => {
     const workflow_callback = (workflow) => {
         const tbody = document.getElementById('tbody');
         const tr = document.createElement('tr');
@@ -138,7 +174,7 @@ process_repos(user, auth, (repo) => {
         const badge_td = document.createElement('td');
         tr.appendChild(badge_td);
         const badge_img = document.createElement('img');
-        // Add a query string param, to prevent using a cached image.
+        // Add a query string param to prevent showing a cached image.
         badge_img.src = workflow.badge_url + '?_=' + String(new Date().getTime());
         badge_td.appendChild(badge_img);
 
@@ -172,7 +208,7 @@ process_repos(user, auth, (repo) => {
                 conclusion_td.appendChild(conclusion_span);
             }
         };
-        process_run(user, repo.name, workflow.id, auth, run_callback);
+        process_run(user, repo.name, workflow.id, repo.default_branch, run_callback);
     };
-    process_workflows(user, repo.name, auth, workflow_callback);
+    process_workflows(user, repo.name, workflow_callback);
 });
